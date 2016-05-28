9f4ef56faa9f35a97ed8ad49fc90a9838543524d
benpillet <ben_pillet@yahoo.com>
2016-05-28 13:09:31 -0700

    Draft of associate_public_ip_address

--- lib/puppet/type/ec2_launchconfiguration.rb
+++ lib/puppet/type/ec2_launchconfiguration.rb
@@ -66,31 +66,36 @@ Puppet::Type.newtype(:ec2_launchconfiguration) do
 
   newproperty(:block_device_mappings, :array_matching => :all) do
     desc "One or more mappings that specify how block devices are exposed to the instance."
-    defaultto []
     validate do |value|
-      Puppet.debug "validate(#{value})"
+      Puppet.warning "validate(#{value})"
       devices = value.is_a?(Array) ? value : [value]
       devices.each do |device|
-        Puppet.debug "device: #{device} keys: #{device.keys} includes: #{device.keys.include?('device_name')}"
-        fail "block device must be named" unless device.keys.include?('device_name')
+        fail "block device must be named" unless value.keys.include?('device_name')
         choices = ['volume_size', 'snapshot_id']
-        fail "block device must include at least one of: " + choices.join(' ') if (device.keys & choices).empty?
-        if device['volume_type'] == 'io1'
-          fail 'must specify iops if using provisioned iops volumes' unless device.keys.include?('iops')
+        fail "block device must include at least one of: " + choices.join(' ') if (value.keys & choices).empty?
+        if value['volume_type'] == 'io1'
+          fail 'must specify iops if using provisioned iops volumes' unless value.keys.include?('iops')
         end
       end
     end
 
     def insync?(is)
-      existing_devices = is.collect { |device| Hash[device.map { |k, v| [k.to_sym, v] }] }
-      specified_devices = should.collect { |device|
-        dev = Hash[device.map { |k, v| [k.to_sym, v] }]
-        dev[:volume_type] ||= 'standard'
-        dev
-      }
-      Puppet.debug "existing_devices: #{existing_devices.to_set.inspect}"
-      Puppet.debug "specified_devices: #{specified_devices.to_set.inspect}"
+      existing_devices = is.collect { |device| device[:device_name] }
+      specified_devices = should.collect { |device| device['device_name'] }
       existing_devices.to_set == specified_devices.to_set
+    end
+
+    def set(value)
+      read_only_warning(value, self, should)
+    end
+  end
+
+  newproperty(:associate_public_ip_address, :boolean => true, :parent => Puppet::Parameter::Boolean) do
+    desc 'Specifies whether to assign a public IP address to each instance launched in a Amazon VPC. If the instance is launched into a default subnet, the default is true.'
+    defaultto :true
+    newvalues(:true, :false)
+    def insync?(is)
+      is.to_s == should.to_s
     end
 
     def set(value)
